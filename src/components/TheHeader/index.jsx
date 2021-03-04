e8a9d233c0e87e2f0379037b967ce004f714ac40
F9Uf <atoseka@gmail.com>
2021-03-04 18:09:56 +0700

    fix TheHeader menu responsive

--- src/components/TheHeader/index.jsx
+++ src/components/TheHeader/index.jsx
@@ -30,13 +30,17 @@ function TheHeader() {
 
   return (
     <header className={"p-5 top-0 fixed w-full z-50 text-white transition duration-1000 ease-in-out " + headerClass}>
-      <div className="menu flex flex-row justify-center items-center space-x-10">
-        <div className="left-menu text-sm">
+      <div className="menu flex flex-col sm:flex-row justify-center items-center sm:space-x-10">
+        <div className="left-menu text-sm w-40 sm:flex justify-end text-center hidden">
           <TheMenus menus={menus.filter(e => e.pos === 'left')}/>
         </div>
-        <h1 className="title font-bold text-xl text-center text-white">CLOTH SHOP</h1>
-        <div className="right-menu text-sm">
+        <h1 className="title font-bold text-xl text-center text-white w-40">CLOTH SHOP</h1>
+        <div className="right-menu text-sm w-40 text-center hidden sm:flex">
           <TheMenus menus={menus.filter(e => e.pos === 'right')}/>
+        </div>
+
+        <div className="responsive-menu sm:hidden flex mt-3 text-center">
+          <TheMenus menus={menus}/>
         </div>
       </div>
     </header>
