edd886b7eb642ff3a2f6d1636ae7d424db50923f
F9Uf <atoseka@gmail.com>
2021-03-04 18:21:01 +0700

    update menu hover underline animation

--- src/components/TheHeader/TheMenus/index.jsx
+++ src/components/TheHeader/TheMenus/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import './index.css'
 
 const TheMenus = ({ menus }) => {
   return (
     <ul className="flex space-x-6">
       {menus && menus
         .map(menu => (
-          <li key={menu.id}>
+          <li key={menu.id} className="menu">
             <a href={menu.link}>{menu.title}</a>
           </li>
         ))
