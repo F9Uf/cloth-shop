230fc85db16a036e5a31d99c71adab7d713e5ebc
F9Uf <atoseka@gmail.com>
2021-03-04 19:40:48 +0700

    create content component

---
+++ src/components/TheContent/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import CardItem from '../CardItem'
+
+const TheContent = () => {
+  return (
+    <div className="content grid grid-flow-row lg:grid-cols-4 grid-cols-2 gap-5 sm:px-28 px-3 py-10">
+      <CardItem />
+      <CardItem />
+      <CardItem />
+      <CardItem />
+      <CardItem />
+      <CardItem />
+      <CardItem />
+      <CardItem />
+      <CardItem />
+      <CardItem />
+    </div>
+  )
+}
+
+export default TheContent
