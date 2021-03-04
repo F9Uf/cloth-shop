7370fddf545b7911c8a5e9d6ca64099d86810816
F9Uf <atoseka@gmail.com>
2021-03-05 00:38:53 +0700

    fetch product from json with id as query params

---
+++ src/components/TheGallery/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const TheGallery = ({ pictures }) => {
+
+  return (
+    <h1>gallery</h1>
+  )
+}
+
+export default TheGallery
--- src/page/ProductPage.jsx
+++ src/page/ProductPage.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Redirect, useParams } from 'react-router-dom'
+import useProduct from '../hooks/product'
+
+import TheGallery from '../components/TheGallery'
+
+import productsJson from '../assets/data.json'
 
 const ProductPage = () => {
+  const [products, setProducts] = useProduct(productsJson);
+  const [product, setProduct] = useState(null);
+  const { id } = useParams();
+  
+  useEffect(() => {
+    const idNumber = parseInt(id)
+    if (idNumber < 0) {
+      return setProduct(null)
+    }
+    const pIndex = products.findIndex(e => e.id === idNumber)
+    if (pIndex < 0) {
+      return setProduct(null)
+    }
+    return setProduct(products[pIndex])
+  }, [id])
 
   return (
-    <h1>product item</h1>
+    <>
+      <div className="bg-black w-full h-20 -mt-3"></div>
+      {
+        product ? (
+          <div className="flex justify-between sm:px-28 px-3 my-5">
+            <div className="w-1/2">
+              <TheGallery pictures={product} />
+            </div>
+            <div className="w-1/2">asdlkfhl</div>
+          </div>
+        ) :
+        (
+          <div className="sm:px-28 px-3 my-20 text-center text-4xl">NOT FOUND</div>
+        )
+      }
+      
+    </>
   )
 }
 
