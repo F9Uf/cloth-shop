ff278b414abce97ffc01f82d79bc8505369f8341
F9Uf <atoseka@gmail.com>
2021-03-04 19:27:52 +0700

    fix: z index

--- src/components/TheNavbar/index.jsx
+++ src/components/TheNavbar/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const TheNavbar = ({ sortByOptions }) => {
   return (
-    <div className="py-3 sm:px-28 px-3 border-b-2 sm:space-x-5 space-y-2 sm:space-y-0 sm:sticky sm:top-16 bg-white">
+    <div className="py-3 sm:px-28 px-3 border-b-2 sm:space-x-5 space-y-2 sm:space-y-0 sm:sticky sm:top-16 bg-white z-10">
       <label htmlFor="sortBy">Sort By:</label>
       <select name="sortBy" id="sortBy" className="font-semibold cursor-pointer focus:outline-none sm:w-48 w-full rounded-sm border-2 py-1 px-2">
         {
