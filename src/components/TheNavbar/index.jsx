f9099c021caf0d2ca7041a05f581c0fd9ca23c55
F9Uf <atoseka@gmail.com>
2021-03-04 18:33:49 +0700

    adjust navbar sticky

--- src/components/TheNavbar/index.jsx
+++ src/components/TheNavbar/index.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const TheNavbar = ({ sortByOptions }) => {
   return (
-    <div className="py-3 px-28 border-b-2 space-x-5">
+    <div className="py-3 sm:px-28 px-3 border-b-2 sm:space-x-5 space-y-2 sm:space-y-0 sm:sticky sm:top-16 bg-white">
       <label htmlFor="sortBy">Sort By:</label>
-      <select name="sortBy" id="sortBy" className="font-semibold cursor-pointer focus:outline-none w-48">
+      <select name="sortBy" id="sortBy" className="font-semibold cursor-pointer focus:outline-none sm:w-48 w-full rounded-sm border-2 py-1 px-2">
         {
           sortByOptions && sortByOptions
           .map(option => (
