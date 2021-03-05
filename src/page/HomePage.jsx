4ce1bd1f0e69fde6b109cda5749612a0404950c3
F9Uf <atoseka@gmail.com>
2021-03-05 22:21:27 +0700

    create function to sort product list by use selected

--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import useProduct from '../hooks/product'
 
 import TheBanner from '../components/TheBanner'
@@ -9,18 +9,33 @@ import productsJson from '../assets/data.json'
 
 const HomePage = () => {
   const [products, setProducts] = useProduct(productsJson);
-  const [sortBy, setSortBy] = useState('3')
+  const [sortBy, setSortBy] = useState('0')
 
   const [sortByOptions, _] = useState([
-    { id: 0, value: '0', text: 'value 0' },
-    { id: 1, value: '1', text: 'value 1' },
-    { id: 2, value: '2', text: 'value 2' },
-    { id: 3, value: '3', text: 'value 3' },
+    { id: 0, value: '0', text: 'Name A-Z' },
+    { id: 1, value: '1', text: 'Name Z-A' },
+    { id: 2, value: '2', text: 'Price Low to High' },
+    { id: 3, value: '3', text: 'Price High to Low' },
   ])
 
   function handlerSortByChange(value) {
     setSortBy(value)
+    let tempProduct = [...products]
+    if (value === '0') {
+      tempProduct = tempProduct.sort((a, b) => a.name.localeCompare(b.name))
+    } else if (value === '1') {
+      tempProduct = tempProduct.sort((a, b) => b.name.localeCompare(a.name))
+    } else if (value === '2') {
+      tempProduct = tempProduct.sort((a, b) => a.price - b.price)
+    } else if (value === '3') {
+      tempProduct = tempProduct.sort((a, b) => b.price - a.price)
+    }
+    setProducts(tempProduct)
   }
+
+  useEffect(() => {
+    handlerSortByChange(sortBy)
+  }, [sortBy])
 
   return (
     <>
@@ -30,7 +45,6 @@ const HomePage = () => {
         sortByChange={handlerSortByChange}
         sortBy={sortBy}
       />
-      {sortBy}
       <TheContent products={products} />
     </>
   )
